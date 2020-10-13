b83e1855abb8fb06c89ae6d7a6e6ec27308eb17b
Derek Baker <derek.baker89@gmail.com>
2020-10-13 10:23:42 +0100

    finished movie rental (inheritance)

--- movie.rb
+++ movie.rb
@@ -3,21 +3,15 @@ class Movie
 
     def initialize(title, days)
         @title = title
-        @days = days
         @total_cost = calculate_rental_cost(days)
+        @rental_points = calculate_rental_points(days)
     end
-
-    # def calculate_rental_cost(days)
-    #     puts "ERROR YOU HAVENT DEFINED FOR THIS CLASS"
-    # end
 
 end
 
 class RegularMovie < Movie
     def initialize(title, days)
         super(title, days)
-        # @total_cost = calculate_rental_cost(days)
-        @rental_points = 1
     end
 
     def calculate_rental_cost(days)
@@ -25,14 +19,17 @@ class RegularMovie < Movie
         total = 2
         total += price_after_2_days*(days - 2) if days > 2 
     end
+
+    def calculate_rental_points(days)
+        1
+    end 
 end
 
 class NewReleaseMovie < Movie
     def initialize(title, days)
         super(title, days)
-        # @total_cost = calculate_rental_cost(days)
-        @rental_points = calculate_rental_points(days)
     end
+
     def calculate_rental_cost(days)
         3*days
     end
@@ -48,8 +45,6 @@ end
 class ChildrenMovie < Movie
     def initialize(title, days) 
         super(title, days)
-        # @total_cost = calculate_rental_cost(days)
-        @rental_points = 1
     end
 
     def calculate_rental_cost(days)
@@ -58,19 +53,8 @@ class ChildrenMovie < Movie
         total += price_after_3_days*(days - 3) if days > 3 
     end
 
+    def calculate_rental_points(days)
+        1
+    end 
+
 end
-
-land_before_time = RegularMovie.new("Land Before Time", 365)
-puts land_before_time.title
-puts land_before_time.rental_points
-puts land_before_time.total_cost
-
-tenent = NewReleaseMovie.new("Tenent", 4)
-puts tenent.title
-puts tenent.rental_points
-puts tenent.total_cost
-
-mulan = ChildrenMovie.new("Mulan (1999)", 5)
-puts mulan.title
-puts mulan.rental_points
-puts mulan.total_cost
---
+++ statement.rb
@@ -0,0 +1,36 @@
+require_relative "movie"
+
+class Statement
+
+    def make_statement(movie_list, customer_name)
+        puts "Rental record for #{customer_name}"
+        rental_points = 0
+        total_cost = 0
+
+        movie_list.each do |movie| 
+            puts "   #{movie.title} £#{movie.total_cost}"
+            rental_points += movie.rental_points
+            total_cost += movie.total_cost
+        end
+
+        puts "You owe £#{total_cost}"
+        puts "You earned #{rental_points} frequent renter points"
+
+    end
+
+
+end
+
+
+land_before_time = RegularMovie.new("Land Before Time", 365)
+
+
+tenent = NewReleaseMovie.new("Tenent", 4)
+
+
+mulan = ChildrenMovie.new("Mulan (1999)", 5)
+
+movie_list = [land_before_time, tenent, mulan]
+
+statement = Statement.new
+statement.make_statement(movie_list, "Derek")
